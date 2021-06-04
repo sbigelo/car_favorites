50f2f4b12424e806c19346866d39a0bc1c5ebb0d
sbigelo <sean.bigelow1@gmail.com>
2021-06-04 14:05:08 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :favorites, only: :index
+  resources :cars, only: :index
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
