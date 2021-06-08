9871b484933fc49333e26201446505ebd3e467a6
sbigelo <sean.bigelow1@gmail.com>
2021-06-08 16:17:36 -0400

    added new seed, and proper needed gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Car.destroy_all
+Favorite.destroy_all
 
 hypercars = Favorite.create(name: "hypercars")
 supercars = Favorite.create(name: "supercars")
-regularcars = Favorite.create(name: "regular cars")
+regularcars = Favorite.create(name: "regularcars")
 
-Car.create(name: "nissan gtr")
+Car.create(name: "nissan gtr", favorite: supercars)
 
+Car.create(name: "toyota", favorite: regularcars)
 
-
-Car.create(name: "mercedes c300")
+Car.create(name: "mercedes c300", favorite: regularcars)
 
